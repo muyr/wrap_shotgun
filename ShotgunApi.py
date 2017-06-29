dbc6516c39b0bc6537c7ceb798e85b95c3487809
muyanru <suibian19881213>
2017-06-29 19:10:35 +0800

    separate Shotgun for singleton use shotgun function directly

--- ShotgunApi.py
+++ ShotgunApi.py
@@ -5,98 +5,15 @@
 # Email : muyanru345@163.com
 ###################################################################
 
-from shotgun_api3 import Shotgun
 import datetime
 import json
 import pkgutil
-import os
-import sys
 from collections import defaultdict
-import ConfigParser
+from ShotgunObj import my_sg
 
 
 class MyNone(object):
     pass
-
-
-class Singleton(type):
-    def __init__(self, *args, **kwargs):
-        super(Singleton, self).__init__(*args, **kwargs)
-        self.__instance = None
-
-    def __call__(self, *args, **kwargs):
-        if self.__instance is None:
-            self.__instance = super(Singleton, self).__call__(*args, **kwargs)
-            return self.__instance
-        else:
-            return self.__instance
-
-
-class ShotgunObj(object):
-    __metaclass__ = Singleton
-
-    def __init__(self, url, login=None, password=None, script_name=None, api_key=None):
-        self._url = url
-        self._login = login
-        self._password = password
-        self._script_name = script_name
-        self._api_key = api_key
-        self._sg = self.connectShotgun()
-
-    def connectShotgun(self):
-        try:
-            if self._login is None:
-                sg = Shotgun(self._url, script_name=self._script_name, api_key=self._api_key)
-            else:
-                sg = Shotgun(self._url, login=self._login, password=self._password)
-            print 'connect succesfully!'
-            return sg
-        except:
-            print 'connect failed!'
-            return None
-
-    def getEntityFields(self, entity):
-        return self._sg.schema_field_read(entity)
-
-    def findEntity(self, entity, filter, fields):
-        return self._sg.find_one(entity, filter, fields)
-
-    def findEntityOneByOne(self, entity, filterList, fields):
-        index = 1
-        summarizeDataDict = self._sg.summarize(entity_type=entity,
-                                               filters=filterList,
-                                               summary_fields=[{'field': 'id', 'type': 'count'}, ])
-        count = summarizeDataDict['summaries']['id']
-        while True:
-            result = self._sg.find(entity, filterList, fields, limit=1, page=index)
-            if result:
-                yield result[0], index, count
-                index += 1
-            else:
-                break
-
-    def findEntities(self, entity, filter, fields):
-        return self._sg.find(entity, filter, fields)
-
-    def createEntity(self, table, dataDict):
-        return self._sg.create(table, dataDict)
-
-    def updateEntity(self, table, sg_id, dataDict):
-        return self._sg.update(table, sg_id, dataDict)
-
-    def upload(self, sgORM, movFile):
-        return self._sg.upload(sgORM.__class__.__name__, sgORM.id, movFile, 'sg_uploaded_movie',
-                               os.path.basename(movFile))
-
-
-class MyShotgun(ShotgunObj):
-    def __init__(self):
-        _config = ConfigParser.SafeConfigParser()
-        _config.read(os.path.realpath(sys.path[0]) + '/sg.conf')
-        url = _config.get('shotgun', 'url')
-        script = _config.get('shotgun', 'script')
-        key = _config.get('shotgun', 'key')
-        super(MyShotgun, self).__init__(url, script_name=script, api_key=key)
 
 
 class FieldBase(object):
@@ -110,8 +27,8 @@ class FieldBase(object):
         if instance is None: return self
         value = instance._cache_data.get(self.field_code, MyNone)
         if value is MyNone:
-            ps = MyShotgun()
-            result = ps.findEntity(instance.__class__.__name__, [['id', 'is', instance._id]], instance._fields_.keys())
+            result = my_sg.find_one(instance.__class__.__name__, [['id', 'is', instance._id]],
+                                    instance._fields_.keys())
             if result is None:
                 raise Exception('Shotgun doesn\'t have this id[%d] %s!' % (instance._id, owner.__name__))
             instance._initData_(result)
@@ -141,7 +58,9 @@ class ProxyEntity(FieldBase):
 
     def __set__(self, instance, value):
         super(ProxyEntity, self).__set__(instance, value)
-        if isinstance(value, dict) or isinstance(value, MyNone) or (value is None):
+        # TODO: more validate
+        # TODO: support SGEntityBase
+        if isinstance(value, (self.field_type, MyNone)) or (value is None):
             instance._cache_data[self.field_code] = value
         else:
             raise TypeError('{} need {}, not {}'.format(self.field_code, 'dict', type(value)))
@@ -155,6 +74,8 @@ class MultiProxyEntity(FieldBase):
 
     def __set__(self, instance, value):
         super(MultiProxyEntity, self).__set__(instance, value)
+        # TODO: more validate
+        # TODO: support SGEntityBase
         if isinstance(value, (list, MyNone)) or (value is None):
             instance._cache_data[self.field_code] = value
         else:
@@ -201,17 +122,20 @@ class SGEntityBase(object):
         return resultDict
 
     def commit(self):
-        ps = MyShotgun()
         resultDict = {}
         for k, v in self._cache_data.items():
             editable = self._fields_.get(k, {'editable': False}).get('editable', True)
             if editable:
                 resultDict[k] = v
-        return ps.updateEntity(self.__class__.__name__, self._id, resultDict)
+        return my_sg.update(self.__class__.__name__, self._id, resultDict)
 
     @classmethod
     def query(cls, *args, **kwargs):
-        ps = MyShotgun()
+        result = cls.query_dict(*args, **kwargs)
+        return [cls.fromShotgunEntity(entity) for entity in result] if result else []
+
+    @classmethod
+    def query_dict(cls, *args, **kwargs):
         filters = []
         if kwargs:
             for k, v in kwargs.items():
@@ -223,8 +147,8 @@ class SGEntityBase(object):
             filters.append([cls._repr_field, 'is', args[0]])
         else:
             raise Exception('one arg, or key=word style!')
-        result = ps.findEntities(cls.__name__, filters, cls._fields_.keys())
-        return [cls.fromShotgunEntity(entity) for entity in result] if result else []
+        result = my_sg.find(cls.__name__, filters, cls._fields_.keys())
+        return result
 
     @classmethod
     def fromString(cls, content):
@@ -247,6 +171,20 @@ class SGEntityBase(object):
             entity = cls(dataDict['id'])
             entity._initData_(dataDict)
             return entity
+
+    @classmethod
+    def getShotgunSchema(cls, clean=True):
+        fields = my_sg.schema_field_read(cls.__name__)
+        if clean:
+            resultDict = {}
+            for name in fields.keys():
+                resultDict[name] = {"field_type": fields[name]['data_type']['value'],
+                                    "field_label": fields[name]['name']['value'],
+                                    "editable": fields[name]['editable']['value']
+                                    }
+            return resultDict
+        else:
+            return fields
 
 
 class Project(SGEntityBase):
@@ -302,22 +240,13 @@ class TimeLog(SGEntityBase):
     _repr_field = 'date'
 
 
-# A tool for author to get shotgun entity's fields
-def _getEntityFields(entity):
-    a = MyShotgun()
-    fields = a.getEntityFields(entity)
-    for name in fields.keys():
-        fieldType = fields[name]['data_type']['value']
-        print name, fieldType, fields[name]['name']['value'], fields[name]['editable']['value']
-
-
 if __name__ == '__main__':
     user = HumanUser.fromString('postgres')
     print user.email
-    for timelog in TimeLog.query(user=user):
-        print timelog
+    # for timelog in TimeLog.query(user=user):
+    #     print timelog
         # print user.commit()
-        # for t in Task.query(task_assignees=user, project=p):
-        #     print t.content, t.entity, t.step, t.task_assignees
+    for t in Task.query_dict(task_assignees=user):
+        print t
         # for p in Status.query():
         #     print p

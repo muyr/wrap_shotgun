ab599c8d8fce80a1cf9b255be83f9a50ae4567d7
muyanru <muyanru345@163.com>
2017-09-01 18:45:39 +0800

    update a lot

--- ShotgunApi.py
+++ ShotgunApi.py
@@ -26,13 +26,14 @@ class FieldBase(object):
     def __get__(self, instance, owner):
         if instance is None: return self
         value = instance._cache_data.get(self.field_code, MyNone)
-        if (value is MyNone) and instance._id:
+        if (value is MyNone) and instance.id:
             _fields = instance._fields_
             sg_fields = [_fields[k].get('inner_code', k) for k in _fields.keys()]
             my_sg = MyShotgun()
-            result = my_sg.find_one(instance._sg_table, [['id', 'is', instance._id]], sg_fields)
+            print 'find_one'
+            result = my_sg.find_one(instance._sg_table, [['id', 'is', instance.id]], sg_fields)
             if result is None:
-                raise Exception('Shotgun doesn\'t have this id[%d] %s!' % (instance._id, owner._sg_table))
+                raise Exception('Shotgun doesn\'t have this id[%d] %s!' % (instance.id, owner._sg_table))
             instance._initData_(result)
             value = instance._cache_data.get(self.field_code, MyNone)
         return value
@@ -52,14 +53,54 @@ class SGField(FieldBase):
             raise TypeError('{} need {}, not {}'.format(self.field_code, self.field_type, type(value)))
 
 
+class FileField(FieldBase):
+    _cached_data = None
+    _fields_List = ['content_type', 'mime-type', 'link_type', 'name',
+                    'local_path', 'local_path_windows', 'local_path_linux', 'local_path_mac', 'local_path_windows',
+                    'local_storage', 'url', 'id', 'type']
+
+    def __get__(self, instance, owner):
+        dataDict = super(FileField, self).__get__(instance, owner)
+        if isinstance(dataDict, self.field_type):
+            self._cached_data = dataDict
+            for f in self._fields_List:
+                setattr(self, f, dataDict.get(f, None))
+            return self
+        else:
+            return None
+
+    def __set__(self, instance, value):
+        super(FileField, self).__set__(instance, value)
+
+    def __repr__(self):
+        return "File{name:'%s', url:'%s'}" % (self.name, self.url)
+
+    def download(self, baseDir=None):
+        if self._cached_data.get('link_type', 'upload') == 'local':
+            return self._cached_data['local_path']
+        if not baseDir:
+            import os
+            baseDir = os.environ['TMP']
+        sg = MyShotgun()
+        return sg.download_attachment(attachment=self._cached_data,
+                                      file_path='%s/%s' % (baseDir, self._cached_data.get('name')))
+
+    def dataDict(self):
+        return self._cached_data
+
+
 class ProxyEntity(FieldBase):
+    _cached_data_dict = {}
+
     def __get__(self, instance, owner):
         dataDict = super(ProxyEntity, self).__get__(instance, owner)
         # and dataDict.has_key('type') and dataDict.has_key('id')
         # 有理由认为，这里从shotgun获取到的是包含了id 和 type 的字典， 要不然就是None
         if isinstance(dataDict, self.field_type):
-            entity = SGEntityBase.fromShotgunEntity(dataDict)
-            return entity
+            keyName = '{0}_{1}_{2}'.format(instance._sg_table, instance.id, self.field_code)
+            if not self._cached_data_dict.has_key(keyName):
+                self._cached_data_dict.update({keyName: SGEntityBase.fromShotgunEntity(dataDict)})
+            return self._cached_data_dict.get(keyName)
         else:
             return None
 
@@ -74,11 +115,15 @@ class ProxyEntity(FieldBase):
 
 
 class MultiProxyEntity(FieldBase):
+    _cached_data_dict = {}
+
     def __get__(self, instance, owner):
         valueList = super(MultiProxyEntity, self).__get__(instance, owner)
         if isinstance(valueList, self.field_type) and valueList:
-            entities = [SGEntityBase.fromShotgunEntity(value) for value in valueList]
-            return entities
+            keyName = '{0}_{1}_{2}'.format(instance._sg_table, instance.id, self.field_code)
+            if not self._cached_data_dict.has_key(keyName):
+                self._cached_data_dict.update({keyName: [SGEntityBase.fromShotgunEntity(value) for value in valueList]})
+            return self._cached_data_dict.get(keyName)
         else:
             return []
 
@@ -94,9 +139,9 @@ class MultiProxyEntity(FieldBase):
 
 class SGMetaClass(type):
     def __new__(cls, name, bases, clsdict):
-        data = pkgutil.get_data('ShotgunSchema', '{}.json'.format(name))
+        data = pkgutil.get_data('MORE_CLOUD.MORE_SG.ShotgunSchema', '{}.json'.format(name))
         fields = json.loads(data)
-        clsdict.update({'_fields_': fields, '_sg_table': name, '_display_name': name})
+        clsdict.update({'_fields_': fields, '_sg_table': name, '_display_name': name, 'type': name})
         for field_code, field_dict in fields.items():
             field_label = field_dict['field_label']
             field_type = field_dict['field_type']
@@ -105,27 +150,33 @@ class SGMetaClass(type):
                 clsdict.update({field_code: ProxyEntity(field_code, field_label, dict, editable)})
             elif field_type == 'MultiProxyEntity':
                 clsdict.update({field_code: MultiProxyEntity(field_code, field_label, list, editable)})
+            elif field_type == 'FileField':
+                clsdict.update({field_code: FileField(field_code, field_label, dict, editable)})
             else:
                 clsdict.update({field_code: SGField(field_code, field_label, eval(field_type), editable)})
         return super(SGMetaClass, cls).__new__(cls, name, bases, clsdict)
 
 
 class SGEntityBase(object):
-    _repr_field = 'name'
-
     def __init__(self, key=None):
-        self._id = key
+        self.id = key
         self._cache_data = defaultdict(MyNone)
 
     def _initData_(self, dataDict):
         for k, v in self._fields_.items():
             attr = v['inner_code'] if v.has_key('inner_code') else k
-            if attr in dataDict.keys():
+            if k in dataDict.keys():
+                self._cache_data[k] = dataDict[k]
+                if v.has_key('inner_code'):
+                    # 如果从sg查询得到外链的结果，有name，没有inner_code，那么把inner_code属性值设置为 name的值
+                    self._cache_data[v['inner_code']] = dataDict[k]
+            elif attr in dataDict.keys():
+                # 如果从sg查询本身得到的结果，没有name，但有name 的 inner_code，那么把name属性值设置为 inner_code的值
                 self._cache_data[k] = dataDict[attr]
 
     def __repr__(self):
-        return "%s{'id':%s, '%s':'%s'}" % (
-            self._display_name, self._id, self._repr_field, getattr(self, self._repr_field, MyNone))
+        return "%s{id:%s, %s:'%s'}" % (
+            self._display_name, self.id, 'name', getattr(self, 'name', None))
 
     def toPyDict(self):
         resultDict = self.toShotgunEntity()
@@ -139,7 +190,7 @@ class SGEntityBase(object):
             if editable:
                 resultDict[k] = v
         my_sg = MyShotgun()
-        return my_sg.update(self._sg_table, self._id, resultDict)
+        return my_sg.update(self._sg_table, self.id, resultDict)
 
     @classmethod
     def query(cls, *args, **kwargs):
@@ -151,18 +202,25 @@ class SGEntityBase(object):
         filters = []
         if kwargs:
             for k, v in kwargs.items():
+                if k == 'id':
+                    filters.append([k, 'is', v])
+                    continue
                 if k not in cls._fields_.keys():
                     raise '{0} has no attribute {1}'.format(cls._sg_table, k)
                 else:
                     key = cls._fields_[k].get('inner_code', k)
-                    value =  v if not isinstance(v, SGEntityBase) else v.toShotgunEntity()
-                    filters.append([key, 'is', value])
-        elif len(args) == 1:
-            filters.append([cls._repr_field, 'is', args[0]])
-        else:
-            raise Exception('one arg, or key=word style!')
+                    if isinstance(v, list):
+                        values = [i if not isinstance(i, SGEntityBase) else i.toShotgunEntity() for i in v]
+                        filters.append([key, 'in', values])
+                    else:
+                        value = v if not isinstance(v, SGEntityBase) else v.toShotgunEntity()
+                        filters.append([key, 'is', value])
+        if len(args) == 1:
+            filters.append([cls._fields_['name'].get('inner_code', 'name'), 'is', args[0]])
         my_sg = MyShotgun()
-        result = my_sg.find(cls._sg_table, filters, cls._fields_.keys())
+        print 'find'
+        sg_fields = [cls._fields_[k].get('inner_code', k) for k in cls._fields_.keys()]
+        result = my_sg.find(cls._sg_table, filters, sg_fields)
         return result
 
     @classmethod
@@ -175,7 +233,7 @@ class SGEntityBase(object):
         return {'id': _id, 'type': cls._sg_table}
 
     def toShotgunEntity(self):
-        return self.__class__.shotgunEntity(self._id)
+        return self.__class__.shotgunEntity(self.id)
 
     @classmethod
     def fromShotgunEntity(cls, dataDict):
@@ -208,32 +266,26 @@ class Project(SGEntityBase):
 
 class Shot(SGEntityBase):
     __metaclass__ = SGMetaClass
-    _repr_field = 'code'
 
 
 class Sequence(SGEntityBase):
     __metaclass__ = SGMetaClass
-    _repr_field = 'code'
 
 
 class Task(SGEntityBase):
     __metaclass__ = SGMetaClass
-    _repr_field = 'content'
 
 
 class Version(SGEntityBase):
     __metaclass__ = SGMetaClass
-    _repr_field = 'code'
 
 
 class Asset(SGEntityBase):
     __metaclass__ = SGMetaClass
-    _repr_field = 'code'
 
 
 class HumanUser(SGEntityBase):
     __metaclass__ = SGMetaClass
-    _repr_field = 'login'
 
 
 class Department(SGEntityBase):
@@ -242,22 +294,18 @@ class Department(SGEntityBase):
 
 class Step(SGEntityBase):
     __metaclass__ = SGMetaClass
-    _repr_field = 'code'
 
 
 class Status(SGEntityBase):
     __metaclass__ = SGMetaClass
-    _repr_field = 'code'
 
 
 class TimeLog(SGEntityBase):
     __metaclass__ = SGMetaClass
-    _repr_field = 'date'
 
 
 class CustomNonProjectEntity01(SGEntityBase):
     __metaclass__ = SGMetaClass
-    _repr_field = 'code'
 
 
 CustomNonProjectEntity01._display_name = 'MyPerson'
@@ -266,28 +314,85 @@ MyPerson = CustomNonProjectEntity01
 
 class CustomEntity02(SGEntityBase):
     __metaclass__ = SGMetaClass
-    _repr_field = 'name'
 
 
 CustomEntity02._display_name = 'ProjectPerson'
 ProjectPerson = CustomEntity02
 
+
+class CustomEntity05(SGEntityBase):
+    __metaclass__ = SGMetaClass
+
+
+CustomEntity05._display_name = 'OtherWorks'
+OtherWorks = CustomEntity05
+
+
+class CustomEntity08(SGEntityBase):
+    __metaclass__ = SGMetaClass
+
+
+CustomEntity08._display_name = 'PublishWorkFile'
+PublishWorkFile = CustomEntity08
+
+
+class CustomEntity09(SGEntityBase):
+    __metaclass__ = SGMetaClass
+
+
+CustomEntity09._display_name = 'PublishElement'
+PublishElement = CustomEntity09
+
+
+class Note(SGEntityBase):
+    __metaclass__ = SGMetaClass
+
+
+class Attachment(SGEntityBase):
+    __metaclass__ = SGMetaClass
+
+
+class LocalStorage(SGEntityBase):
+    __metaclass__ = SGMetaClass
+
+
 if __name__ == '__main__':
-    # user = HumanUser.fromString('postgres')
+    # user = HumanUser.fromString('xiaoao guo')
     # print user.name
-    # Task.query_dict(task_assignees=user)
-    # task = Task(24779)
-    # print task.task_assignees
-    # t = Task()
-    # t.content = 'test'
-    # print t
-    # i = MyPerson.fromString('muyanru')
-    # p = Project.fromString()
-    # ProjectPerson.query(name='muyanru', project=)
-    # print i.sg_project_people
-    # print i._display_name
-    # seq = Sequence.fromString('pl')
-    # prj = Project.fromString('wkz-test')
-    # print seq, prj
-    # print Task.query(sg_asset_type='prp', project=prj, sg_status_list='ip')
-    print Task.query(sequence='pl')
+    prj = Project.fromString('other')
+    for i in Task.query(project=prj):
+        print i.entity
+        # seq = Sequence.fromString('pl')
+        # sg = MyShotgun()
+        # v = sg.find_one('Version', [['id', 'is', 28869]], ['image'])
+        # print v
+        # v = Version(28869)
+        # print v.sg_uploaded_movie
+        # print v.image
+        # print v.sg_uploaded_movie.download()
+        # print v.entity
+        # a = Attachment.fromFileField(v.sg_uploaded_movie)
+        # n = Note(11553)
+        # for i in n.attachments:
+        #     print i.this_file.download('d:/mu_data')
+        # print a.download(baseDir='d:/mu_data')
+        # print user
+        # t = Task.query(task_assignees=user)[0]
+        # print t.entity
+        # print t.entity
+        # task = Task(24779)
+        # print task.task_assignees
+        # t = Task()
+        # t.content = 'test'
+        # print t
+        # i = MyPerson.fromString('muyanru')
+        # p = Project.fromString()
+        # ProjectPerson.query(name='muyanru', project=)
+        # print i.sg_project_people
+        # print i._display_name
+        # seq = Sequence.fromString('pl')
+        # prj = Project.fromString('wkz-test')
+        # print seq, prj
+        # print Task.query(sg_asset_type='prp', project=prj, sg_status_list='ip')
+        # print Task.query(sequence=seq)[0].upstream_tasks
+        # print Status.query_dict()
--- ShotgunObj.py
+++ ShotgunObj.py
@@ -5,6 +5,7 @@
 # Email : muyanru345@163.com
 ###################################################################
 
+
 from shotgun_api3 import Shotgun
 import ConfigParser
 import sys
@@ -26,6 +27,7 @@ class Singleton(type):
 
 class ShotgunObj(Shotgun):
     __metaclass__ = Singleton
+
     def __init__(self, base_url, script_name, api_key):
         super(ShotgunObj, self).__init__(base_url,
                                          script_name=script_name,
@@ -47,12 +49,16 @@ class ShotgunObj(Shotgun):
 
 
 class MyShotgun(ShotgunObj):
-    _config = ConfigParser.SafeConfigParser()
-    _config.read(os.path.realpath(sys.path[0]) + '/sg.conf')
-    url = _config.get('shotgun', 'url')
-    script = _config.get('shotgun', 'script')
-    key = _config.get('shotgun', 'key')
+    import pkgutil, json
+    data = pkgutil.get_data('wrap_shot', 'SG_CONFIG.json')
+    settingDict = json.loads(data)
+    url = settingDict.get('shotgun').get('url')
+    script = settingDict.get('shotgun').get('script')
+    key = settingDict.get('shotgun').get('key')
 
     def __init__(self):
         super(MyShotgun, self).__init__(self.url, self.script, self.key)
 
+
+if __name__ == '__main__':
+    my_sg = MyShotgun()

f7b6af4bca8dccae12bbbd6612a964f3e739256c
Yanru Mu <muyanru345@163.com>
2018-09-13 16:50:30 +0800

    refactor

--- ShotgunObj.py
+++ ShotgunObj.py
@@ -4,41 +4,40 @@
 # Date  : 2017.6
 # Email : muyanru345@163.com
 ###################################################################
+__all__ = ['MyShotgun']
 
 from shotgun_api3 import Shotgun
-import ConfigParser
-import sys
-import os
 
 
 class Singleton(type):
-    def __init__(self, *args, **kwargs):
-        super(Singleton, self).__init__(*args, **kwargs)
-        self.__instance = None
+    def __init__(cls, *args, **kwargs):
+        super(Singleton, cls).__init__(*args, **kwargs)
+        cls.__instance = None
 
-    def __call__(self, *args, **kwargs):
-        if self.__instance is None:
-            self.__instance = super(Singleton, self).__call__(*args, **kwargs)
-            return self.__instance
+    def __call__(cls, *args, **kwargs):
+        if cls.__instance is None:
+            cls.__instance = super(Singleton, cls).__call__(*args, **kwargs)
+            return cls.__instance
         else:
-            return self.__instance
+            return cls.__instance
 
 
 class ShotgunObj(Shotgun):
     __metaclass__ = Singleton
+
     def __init__(self, base_url, script_name, api_key):
         super(ShotgunObj, self).__init__(base_url,
                                          script_name=script_name,
                                          api_key=api_key)
 
-    def find_one_by_one(self, entity, filterList, fields):
+    def find_one_by_one(self, entity, filter_list, field_list):
         index = 1
-        summarizeDataDict = self.summarize(entity_type=entity,
-                                           filters=filterList,
-                                           summary_fields=[{'field': 'id', 'type': 'count'}, ])
-        count = summarizeDataDict['summaries']['id']
+        summarize_data_dict = self.summarize(entity_type=entity,
+                                             filters=filter_list,
+                                             summary_fields=[{'field': 'id', 'type': 'count'}, ])
+        count = summarize_data_dict['summaries']['id']
         while True:
-            result = self.find(entity, filterList, fields, limit=1, page=index)
+            result = self.find(entity, filter_list, field_list, limit=1, page=index)
             if result:
                 yield result[0], index, count
                 index += 1
@@ -47,12 +46,11 @@ class ShotgunObj(Shotgun):
 
 
 class MyShotgun(ShotgunObj):
-    _config = ConfigParser.SafeConfigParser()
-    _config.read(os.path.realpath(sys.path[0]) + '/sg.conf')
-    url = _config.get('shotgun', 'url')
-    script = _config.get('shotgun', 'script')
-    key = _config.get('shotgun', 'key')
-
     def __init__(self):
-        super(MyShotgun, self).__init__(self.url, self.script, self.key)
+        import config
+        super(MyShotgun, self).__init__(config.URL, config.SCRIPT, config.KEY)
 
+
+if __name__ == '__main__':
+    sg = MyShotgun()
+    print sg.find('Project', [['name', 'is', 'dayu-demo']])
